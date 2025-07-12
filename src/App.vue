8854523372a232d0b7f99579f8559053a5746db4
Kurnia Ramadhan <96585178+blogramadhan@users.noreply.github.com>
2025-07-12 20:00:34 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+  import { ref } from 'vue';
+
+  const product = ref('Boots')
+</script>
+
+<template>
+  <div class="product-display">
+    <div class="product-container">
+      <div class="product-info">
+        <h1>{{ product }}</h1>
+      </div>
+    </div>
+  </div>
+</template>
